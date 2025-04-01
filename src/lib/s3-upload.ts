1f01dc283b3284fc295f2a2ad18462ad347da529
Arnav Bansal <bansalarnav221@gmail.com>
2025-04-01 20:50:16 +0530

    Update s3-upload.ts

--- src/lib/s3-upload.ts
+++ src/lib/s3-upload.ts
@@ -30,7 +30,7 @@ export async function uploadToS3(file: File, onProgress?: UploadProgressCallback
 
         // Set up upload parameters
         const params = {
-            Bucket: process.env.NEXT_AWS_BUCKET_NAME,
+            Bucket: process.env.NEXT_AWS_BUCKET_NAME as string,
             Key: fileName,
             Body: file,
             ContentType: file.type,
